894efc8876d9e7472d4749608d7d17ef98db3cfd
CarlosRamosDeveloper <CarlosRamosDev@protonmail.com>
2024-08-12 21:29:14 +0200

    Added RequestMapping for cleaner code

--- src/main/java/org/carlosramosdev/curso/springboot/webapp/webapp/controllers/UserRestController.java
+++ src/main/java/org/carlosramosdev/curso/springboot/webapp/webapp/controllers/UserRestController.java
@@ -1,15 +1,17 @@
 package org.carlosramosdev.curso.springboot.webapp.webapp.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api")
 public class UserRestController {
 
-    @GetMapping("/api/details")
+    @GetMapping("/details")
     public Map<String, Object> details(){
         Map<String, Object> json = new HashMap<>();
         json.put("title", "Hola Mundo Spring Boot");
