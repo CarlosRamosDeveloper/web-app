19d647e2065d7785afdfec8253c05fc6ce331445
CarlosRamosDeveloper <CarlosRamosDev@protonmail.com>
2024-08-12 20:17:41 +0200

    userController commit

--- src/main/java/org/carlosramosdev/curso/springboot/webapp/webapp/controllers/UserController.java
+++ src/main/java/org/carlosramosdev/curso/springboot/webapp/webapp/controllers/UserController.java
@@ -1,5 +1,15 @@
 package org.carlosramosdev.curso.springboot.webapp.webapp.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+
+    @GetMapping("/details")
+    public String details(){
+
+        return "details";
+    }
 
 }
