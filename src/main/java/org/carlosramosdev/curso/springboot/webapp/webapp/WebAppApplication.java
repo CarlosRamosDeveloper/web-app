5e7a731ebc12143057d3825f6c5633b57fadfed7
CarlosRamosDeveloper <CarlosRamosDev@protonmail.com>
2024-08-12 18:54:26 +0200

    1st commit

---
+++ src/main/java/org/carlosramosdev/curso/springboot/webapp/webapp/WebAppApplication.java
@@ -0,0 +1,13 @@
+package org.carlosramosdev.curso.springboot.webapp.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppApplication.class, args);
+	}
+
+}
