38c052152c2a4f9e8762022225e459317770d04f
CarlosRamosDeveloper <CarlosRamosDev@protonmail.com>
2024-08-12 21:04:09 +0200

    added atributes to model

--- src/main/java/org/carlosramosdev/curso/springboot/webapp/webapp/controllers/UserController.java
+++ src/main/java/org/carlosramosdev/curso/springboot/webapp/webapp/controllers/UserController.java
@@ -1,13 +1,18 @@
 package org.carlosramosdev.curso.springboot.webapp.webapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
     @GetMapping("/details")
-    public String details(){
+    public String details(Model model){
+        model.addAttribute("title", "Hola Mundo Spring Boot");
+        model.addAttribute("name","Carlos");
+        model.addAttribute("lastname","Ramos");
+        model.addAttribute("ocupation","Developer");
 
         return "details";
     }
