0993e3e64ddd33cfe49110fc65ab9bce3b95ecf0
CarlosRamosDeveloper <CarlosRamosDev@protonmail.com>
2024-08-13 16:04:47 +0200

    Implemented UserDto in UserRestController

--- src/main/java/org/carlosramosdev/curso/springboot/webapp/webapp/controllers/UserRestController.java
+++ src/main/java/org/carlosramosdev/curso/springboot/webapp/webapp/controllers/UserRestController.java
@@ -1,6 +1,7 @@
 package org.carlosramosdev.curso.springboot.webapp.webapp.controllers;
 
 import org.carlosramosdev.curso.springboot.webapp.webapp.models.User;
+import org.carlosramosdev.curso.springboot.webapp.webapp.models.dto.UserDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,16 @@ import java.util.Map;
 public class UserRestController {
 
     @GetMapping("/details")
-    public Map<String, Object> details(){
+    public UserDto details(){
+        UserDto userDto = new UserDto();
+        User user = new User("Carlos", "Ramos", "Developer");
+        userDto.setUser(user);
+        userDto.setTitle("Hola Mundo Spring Boot");
+        return userDto;
+    }
+
+    @GetMapping("/details.map")
+    public Map<String, Object> detailsMap(){
         User user = new User("Carlos", "Ramos", "Developer");
         Map<String, Object> json = new HashMap<>();
         json.put("title", "Hola Mundo Spring Boot");
