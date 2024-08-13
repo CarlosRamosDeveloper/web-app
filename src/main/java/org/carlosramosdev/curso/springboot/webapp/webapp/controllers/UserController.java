30fc1a8d1628e1c619c9fa85a8b337b62b64e608
CarlosRamosDeveloper <CarlosRamosDev@protonmail.com>
2024-08-13 17:00:19 +0200

    Added mapping -> list. Added list template view

--- src/main/java/org/carlosramosdev/curso/springboot/webapp/webapp/controllers/UserController.java
+++ src/main/java/org/carlosramosdev/curso/springboot/webapp/webapp/controllers/UserController.java
@@ -3,7 +3,11 @@ package org.carlosramosdev.curso.springboot.webapp.webapp.controllers;
 import org.carlosramosdev.curso.springboot.webapp.webapp.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -16,6 +20,14 @@ public class UserController {
         model.addAttribute("user",user);
 
         return "details";
+    }
+
+    @GetMapping("/list")
+    public String list(ModelMap model) {
+        List<User> users = new ArrayList<>();
+        model.addAttribute("users", users);
+
+        return "list";
     }
 
 }
