ab28d7a0ad7cf5edc3a9be69d49369e38c816ae6
CarlosRamosDeveloper <CarlosRamosDev@protonmail.com>
2024-08-14 16:35:46 +0200

    Created mix endpoint

--- src/main/java/org/carlosramosdev/curso/springboot/webapp/webapp/controllers/PathVariableController.java
+++ src/main/java/org/carlosramosdev/curso/springboot/webapp/webapp/controllers/PathVariableController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/var")
 public class PathVariableController {
@@ -14,5 +17,15 @@ public class PathVariableController {
         ParamDto param = new ParamDto();
         param.setMessage(message);
         return param;
+    }
+
+    @GetMapping("/mix/{product}/{id}")
+    public Map<String, Object> mixPathVar(@PathVariable String product, @PathVariable long id){
+
+        Map<String, Object> json = new HashMap<>();
+        json.put("product", product);
+        json.put("id", id);
+
+        return json;
     }
 }
