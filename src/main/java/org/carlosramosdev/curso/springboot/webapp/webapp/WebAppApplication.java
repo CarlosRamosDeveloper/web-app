330e970e3a1e561e8748ecaac0f90fc609472354
CarlosRamosDeveloper <CarlosRamosDev@protonmail.com>
2024-08-14 18:37:19 +0200

    Migrated app configuration to ValuesConfig

--- src/main/java/org/carlosramosdev/curso/springboot/webapp/webapp/WebAppApplication.java
+++ src/main/java/org/carlosramosdev/curso/springboot/webapp/webapp/WebAppApplication.java
@@ -2,10 +2,8 @@ package org.carlosramosdev.curso.springboot.webapp.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:values.properties")
 public class WebAppApplication {
 
 	public static void main(String[] args) {
