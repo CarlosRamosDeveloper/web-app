80bbcd848af9aeb8df78f7520198ac45a664db56
CarlosRamosDeveloper <CarlosRamosDev@protonmail.com>
2024-08-14 18:18:08 +0200

    Migrated data from application.properties to values.properties. Implemented values.properties into WebAppApplication

--- src/main/java/org/carlosramosdev/curso/springboot/webapp/webapp/WebAppApplication.java
+++ src/main/java/org/carlosramosdev/curso/springboot/webapp/webapp/WebAppApplication.java
@@ -2,8 +2,10 @@ package org.carlosramosdev.curso.springboot.webapp.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:values.properties")
 public class WebAppApplication {
 
 	public static void main(String[] args) {
